cce00846c66631844a40d6e6d6abc5a7db9c7ffc
sellen19 <setiawansellen12@gmail.com>
2025-01-19 12:07:22 +0700

    Add files via upload

---
+++ Home SMK/HOME JS/FaktadanData.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.count');
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-count');
+            const count = +counter.innerText;
+            const increment = target / 100;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCount, 20);
+            } else {
+                counter.innerText = target;
+            }
+        };
+        updateCount();
+    });
+});
